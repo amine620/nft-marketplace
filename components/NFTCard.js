2121f7aa5ba02946747774ae234bcb52935c1ef2
amine620 <mohafidi1998@gmail.com>
2022-04-05 10:50:18 -1200

    display images and icon

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,9 +1,15 @@
 import { View, Image , Text} from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import {COLORS,SIZES,SHADOWS,assets} from '../constants'
+import { CricleButton } from './Button'
+
 
 const NFTCard = ({data}) => {
     const navigation=useNavigation()
+
+    const handlePress=()=>{
+
+    }
   return (
     <View style={{
         backgroundColor:COLORS.white,
@@ -12,7 +18,17 @@ const NFTCard = ({data}) => {
         margin:SIZES.base,
         ...SHADOWS.dark
     }}>
-        <Text>NFT</Text>
+        <View style={{width:"100%",height:250}} >
+          <Image source={data.image} 
+          style={{
+              width:"100%",
+              height:"100%",
+              borderTopLeftRadius:SIZES.font,
+              borderTopRightRadius:SIZES.font
+          }}
+          />
+          <CricleButton imgUrl={assets.heart} right={10} top={10} handlePress={handlePress} />
+        </View>
     </View>
   )
 }
--- components/index.js
+++ components/index.js
@@ -1,7 +1,10 @@
+import { RectButton } from "react-native-gesture-handler";
+import { CricleButton } from "./Button";
 import FocusedStatusBar from "./FocusedStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 
 export {
-    HomeHeader,NFTCard,FocusedStatusBar
+RectButton,CricleButton,
+HomeHeader,NFTCard,FocusedStatusBar,
 }
